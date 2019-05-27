0f089f591a0ed193921fb35087bde7512143ff4a
gagaero <49010114+gagaero@users.noreply.github.com>
2019-05-26 23:08:19 -0300

    2

---
+++ src/extracted method.java
@@ -0,0 +1,23 @@
+private double amountFor(Rental each){
+    double result = 0;
+    //determine amounts for each line
+ 
+       thisAmount = amountFor(each);
+ 
+       // add frequent renter points
+       frequentRenterPoints ++;
+       // add bonus for a two day new release rental
+       if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+          each.getDaysRented() > 1) frequentRenterPoints ++;
+ 
+       //show figures for this rental
+       result += "\t" + each.getMovie().getTitle()+ "\t" +
+           String.valueOf(thisAmount) + "\n";
+       totalAmount += thisAmount;
+    } 
+    //add footer lines
+    result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
+    result += "You earned " + String.valueOf(frequentRenterPoints) +
+             " frequent renter points";
+    return result;
+ }
