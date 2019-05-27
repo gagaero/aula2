14596c7fbd25e504616c6fa5e972708a1bf0daa3
gagaero <49010114+gagaero@users.noreply.github.com>
2019-05-26 23:14:36 -0300

    a

--- src/Rental.java
+++ src/Rental.java
@@ -1,15 +1,7 @@
-class Rental {
-    private Movie _movie;
-    private int _daysRented;
-
-    public Rental(Movie movie, int daysRented) {
-      _movie = movie;
-      _daysRented = daysRented;
-    }
-    public int getDaysRented() {
-      return _daysRented;
-    }
-    public Movie getMovie() {
-      return _movie;
-    }
-}
+class Rental...
+   int getFrequentRenterPoints() {
+       if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+          return 2;
+       else
+          return 1;
+   }
