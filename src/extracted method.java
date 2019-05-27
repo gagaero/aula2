e4e07e49a3c51ee58672cbbdde7e9c892b0e9c06
gagaero <49010114+gagaero@users.noreply.github.com>
2019-05-26 23:13:58 -0300

    a

--- src/extracted method.java
+++ src/extracted method.java
@@ -1,8 +1,10 @@
-private double amountFor(Rental each){
-    double result = 0;
-    //determine amounts for each line
- 
-    thisAmount = each.getCharge();
+public String statement() {
+    double totalAmount = 0;
+    int frequentRenterPoints = 0;
+    Enumeration rentals = _rentals.elements();
+    String result = "Rental Record for " + getName() + "\n";
+    while (rentals.hasMoreElements()) {
+       Rental each = (Rental) rentals.nextElement();
  
        // add frequent renter points
        frequentRenterPoints ++;
@@ -10,14 +12,16 @@ private double amountFor(Rental each){
        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
           each.getDaysRented() > 1) frequentRenterPoints ++;
  
-       //show figures for this rental
-       result += "\t" + each.getMovie().getTitle()+ "\t" +
-           String.valueOf(thisAmount) + "\n";
-       totalAmount += thisAmount;
-    } 
-    //add footer lines
+       // show figures for this rental
+       result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf
+          (each.getCharge()) + "\n";
+       totalAmount += each.getCharge();
+ 
+    }
+    
+    // add footer lines
     result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
-    result += "You earned " + String.valueOf(frequentRenterPoints) +
-             " frequent renter points";
+    result += "You earned " + String.valueOf(frequentRenterPoints)
+               + " frequent renter points";
     return result;
  }
